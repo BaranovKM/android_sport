f3b1a38fb56d3124d02e86d6e53edc20004ab645
Konstantin <test@test.ru>
2016-08-24 21:01:15 +0400

    основные классы приложения

---
+++ java/com/konstantin/sport/CardioActivity.java
@@ -0,0 +1,15 @@
+package com.konstantin.sport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardioActivity extends AppCompatActivity {
+/*
+Все что связано с бегом и кардио-нагрузками
+ */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardio_layout);
+    }
+}
